023330944a4fe8c3e4375dbc019a900801ceac0e
Tanvir Ahammed <tanvircolorful@gmail.com>
2025-07-10 13:27:49 +0600

    Creating JSON response - all elements

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from . models import Movie
+from django.http import JsonResponse
 
 # Create your views here.
+def movie_list(request):
+    movies = Movie.objects.all()
+    # print(movies.values())
+    data = {
+        'movies': list(movies.values())
+    }
+    return JsonResponse(data)
--- watchmate/urls.py
+++ watchmate/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('whatchlist_app.urls')),
+    path('movie/', include('watchlist_app.urls')),
 ]
