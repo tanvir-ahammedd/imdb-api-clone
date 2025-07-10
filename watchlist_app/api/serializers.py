2ea0c31a1473ea966128c734cd1aad98b508e4db
Tanvir Ahammed <tanvircolorful@gmail.com>
2025-07-10 21:44:08 +0600

    Serializers - POST, PUT, DELETE

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -13,3 +13,13 @@ class MovieSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=100)
     description = serializers.CharField()
     active = serializers.BooleanField()
+    
+    def create(self, validate_data):
+        return Movie.objects.create(**validate_data)
+    
+    def update(self, instance, validate_data):
+        instance.name = validate_data.get('name', instance.name)
+        instance.description = validate_data.get('description', instance.description)
+        instance.active = validate_data.get('active', instance.active)
+        return instance
+        
