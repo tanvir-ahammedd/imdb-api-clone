07a4d03c074c11b8ca176fe30b394191c070d554
Tanvir Ahammed <tanvircolorful@gmail.com>
2025-07-10 14:12:46 +0600

    Creating JSON response - individual elements

--- watchlist_app/urls.py
+++ watchlist_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list/', views.movie_list, name="movie-list")
+    path('list/', views.movie_list, name="movie-list"),
+    path('list/<int:pk>', views.movie_data, name="movie-data"),
 ]
--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from . models import Movie
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
 
 # Create your views here.
 def movie_list(request):
     movies = Movie.objects.all()
-    # print(movies.values())
     data = {
         'movies': list(movies.values())
     }
     return JsonResponse(data)
+
+def movie_data(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    # manual dictionary
+    # data = {
+    #     'name': movie.name,
+    #     'description': movie.description,
+    #     'activ': movie.active
+    # }
+    data = {
+        'movie': model_to_dict(movie)
+    }
+    return JsonResponse(data)
+    
