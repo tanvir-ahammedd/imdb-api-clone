4ea30d16a54ac45e3a51bd8470433c91e38f8b3e
Tanvir Ahammed <tanvircolorful@gmail.com>
2025-07-15 17:54:33 +0600

    Three types of validators

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -7,10 +7,17 @@ from watchlist_app.models import Movie
 #         model = Movie
 #         fields = "__all__"
 
+#custom validator
+def name_length(value):
+    if len(value) < 2:
+        raise serializers.ValidationError("Name is too short")
+    return value
+    
+
 # without Modelserializer -> serializer
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=100)
+    name = serializers.CharField(validators=[name_length])
     description = serializers.CharField()
     active = serializers.BooleanField()
     
@@ -23,4 +30,16 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validate_data.get('active', instance.active)
         instance.save()
         return instance
-        
+    
+    #object level validation
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Title and Description should not be same")
+        return data
+    
+    #field level validation
+    #format: validate_(name of that field)
+    # def validate_name(self, value):
+    #     if len(value) < 2:
+    #         raise serializers.ValidationError("Name is too short")
+    #     return value
